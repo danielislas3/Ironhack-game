e10a0c124502d4a6f1028d192b1f2e36d51c0d0a
Daniel Orio <danielislas3@live.com>
2019-06-18 16:59:56 -0500

    prieba animacion ostrich

--- js/class.js
+++ js/class.js
@@ -20,3 +20,43 @@ class Board {
   }
 }
 
+class Ostrich {
+  constructor() {
+    this.x = 5
+    this.y = canvas.height - 50
+    this.width = 50
+    this.height = 50
+    
+    this.standDer = new Image()
+    
+
+   
+    this.standDer.src = images.ostrichs.standDer
+  }
+  draw() {
+    this.y += 2
+    if (frames % 5 === 0) {
+      this.img =
+        animateHelper === 0 ? this.flappyUp :
+        animateHelper === 1 ? this.flappyMid :
+        animateHelper === 2 ? this.flappyDown : this.flappyMid
+      if (animateHelper < 4) {
+        animateHelper++
+      } else {
+        animateHelper = 0
+      }
+    }
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+  }
+  flap() {
+    console.log("ADSasdas");
+
+    this.y -= 25
+  }
+  isTouching(pipe) {
+    return (this.x < pipe.x + pipe.width &&
+      this.x + this.width > pipe.x &&
+      this.y > pipe.y + pipe.height &&
+      this.y + pipe.height > pipe.y)
+  }
+}
--- js/variables.js
+++ js/variables.js
@@ -1,11 +1,28 @@
+let frames = 0
+let gameStarted = false
+const keys = []
+const friction = 0.75
+const gravity = 0.98
+const platforms = []
+const platform_width = 120
+const platform_height = 10
+let interval
+
 const images={
     niveles:{
       bgAmanecer: "./img/tarde.png",
       bgMedioDia: "./img/medio-dia-png.jpg",
       bgTarde: "./img/tarde.png",
       bgNoche: "./img/noche.png",
-    }
+    },
+   ostrichs:{
+     caminandoDer:"./img/thumbnail_Caminando.png",
+     caminandoIz:"./img/thumbnail_CaminandoIz.png",
+     brincoDer:"./img/thumbnail_brinco.png",
+     brincoIz:"./img/thumbnail_brincoIz.png",
+     standDer:"./img/standD.png"
+
+   }
   }
 
-let frames = 0
-let interval
+
